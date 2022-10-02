7da29b72f7a58aeaf83fee4a7804de3397ee3b05
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-10-02 14:21:37 +0200

    Add `merge_pdf` function

---
+++ merge_pdf.py
@@ -0,0 +1,31 @@
+import os
+from typing import Optional, List
+
+from PyPDF2 import PdfMerger
+
+def merge_pdf(files: Optional[List[str]] = None,
+              output: Optional[str] = None) -> bool:
+    if files is None or not files:
+        files = []
+        for filename in os.listdir():
+            if filename.endswith('.pdf'):
+                files.append(filename)
+        files.sort(key = str.lower)
+
+    if len(files) > 1:
+        merger = PdfMerger()
+
+        for pdf in files:
+            merger.append(pdf)
+
+        if output is None or not output:
+            output = f'{files[0][:-4]}-merged.pdf'
+        else:
+            if not output.endswith('.pdf'):
+                output = f'{output}.pdf'
+
+        merger.write(output)
+        merger.close()
+        return True
+    else:
+        return False
