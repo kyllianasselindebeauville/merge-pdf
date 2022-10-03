1e783f280500bf6907e011621614dcf01d54fe96
Kyllian Asselin de Beauville <kyllianasselindebeauville@gmail.com>
2022-10-03 21:21:05 +0200

    Move `merge_pdf` to pdf.py

---
+++ merge_pdf/pdf.py
@@ -0,0 +1,50 @@
+import os
+from typing import Optional, List
+
+from PyPDF2 import PdfMerger
+
+
+def get_pdf_files(files: Optional[List[str]] = None) -> List[str]:
+    pdf_files = []
+
+    if files:
+        for filename in files:
+            if filename.endswith('.pdf'):
+                pdf_files.append(filename)
+            elif os.path.exists(f'{filename}.pdf'):
+                pdf_files.append(f'{filename}.pdf')
+    else:
+        for filename in os.listdir():
+            if filename.endswith('.pdf'):
+                pdf_files.append(filename)
+        pdf_files.sort(key=str.lower)
+
+    return pdf_files
+
+
+def get_output_filename(pdf_files: List[str], output: Optional[str]) -> str:
+    if output:
+        if not output.endswith('.pdf'):
+            output = f'{output}.pdf'
+    else:
+        output = f'{pdf_files[0][:-4]}-merged.pdf'
+
+    return output
+
+
+def merge_pdf(files: Optional[List[str]] = None,
+              output: Optional[str] = None) -> bool:
+    pdf_files = get_pdf_files(files)
+
+    if len(pdf_files) > 1:
+        merger = PdfMerger()
+        output_filename = get_output_filename(pdf_files, output)
+
+        for pdf in pdf_files:
+            merger.append(pdf)
+
+        merger.write(output_filename)
+        merger.close()
+        return True
+    else:
+        return False
